eb1e41b0dc22d79da67e80e5a382d5a0a366ed99
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-07-24 13:54:36 -0400

    chart working with react now

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import { Routes, Route } from 'react-router-dom'
 import ViewLeagues from "../ViewLeagues/ViewLeagues"
+import LeaguePage from "../LeaguePage/LeaguePage"
 
 
 export default function App(){
     return(
         <Routes>
             <Route path="/" element={<ViewLeagues/>}></Route>
+            <Route path="/:leaguecode" element={<LeaguePage/>}></Route>
         </Routes>
 
     )
